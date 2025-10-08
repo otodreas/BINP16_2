6d5539b046d17612029ae3214b9df8bc4233f392
otodreas <159846818+otodreas@users.noreply.github.com>
2025-10-08 18:01:53 +0200

    added exercise pdf, updated filenames etc

---
+++ Lecture3/Exercise3.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#%%
+"""
+Section 3.1
+"""
+
+#%% 3.1.6
+# write a script that identifies the user, greets the user. be more positive
+# if i am the user
+
