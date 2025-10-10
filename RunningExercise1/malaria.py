c0c60dc7425627e521062732175bde1f740c21b1
otodreas <159846818+otodreas@users.noreply.github.com>
2025-10-10 17:23:23 +0200

    cleaned up script, threw errors

--- RunningExercise1/malaria.py
+++ RunningExercise1/malaria.py
@@ -1,25 +1,79 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct 10 10:17:40 2025
+The program malaria.py is a command line tool that takes arguments in the
+following format:
+    
+    Script name (malaria.py, unless renamed)
+    FASTA file
+    Tab-delimited BLAST data file
+    File path to a text file where the program's output will be written
+    
+To run the program, type 'python' or 'python3' in your terminal, followed by
+the 4 arguments above, in order.
 
-@author: inf-33-2025
+Written by Oliver Todreas for RunningExerciseI in the course BINP16.
 """
 
 
-# Import libraries
+# Import libraries.
 import sys
+import os
 
-
-# Save command line inputs to variables
+# Save command line inputs to variables.
 try:
     script_name, fasta_file, blast_file, output_path = sys.argv[0:5]
 
 # Exit the program and throw an error if more or fewer than 4 arguments are
 # passed to the script.
 except ValueError:
-    sys.exit('\nError: please enter a fasta file, blast file, and output path '
-             'after the script name\n')
+    sys.exit('\nError: too few or too many arguments passed.\n'
+             'Please pass a FASTA file, a tab-delimited BLAST file, and '
+             'output path after the script name.\n')
+
+# Check that fasta_file is a FASTA file. '.txt' files are not accepted.
+fasta_exts = ('.fasta', '.fas', '.fa', '.fna', '.ffn', '.faa', '.mpfa', '.frn')
+if not fasta_file.endswith(fasta_exts):
+    sys.exit('\nError: a non-FASTA file has been passed to the program.\n')
+        
+
+# Check that output_path is a text file.
+if not output_path.endswith(('.txt')):
+    sys.exit('\nError: output file must be of type .txt\n')
+
+# Check that the FASTA and BLAST files exist. Create empty list
+# non_existent_files to store non existent filepaths in.
+non_existent_files = []
+
+# Loop through file paths passed to the program, checking if they exist and
+# appending them to non_existent_files if they do not.
+for file in [fasta_file, blast_file]:
+    if not os.path.exists(file):
+        non_existent_files.append(file)
+
+# Print the non existent file paths if there are any and interrupt the program,
+# controlling for grammar.
+if len(non_existent_files) > 0:
+    if len(non_existent_files) == 1:
+        print('\nError: the following file does not exist:')
+    else:
+        print('\nError: the following files do not exist:')
+        
+    for i, file in enumerate(non_existent_files):
+        if i < len(non_existent_files) - 1:
+            print(file)
+        else:
+            sys.exit(file + '\n')
+
+# Ensure that the BLAST data file is tab delimited. Read only the first line to
+# save compute, since every line needs to be tab delimited for the program to
+# work.
+with open(blast_file, 'r') as blast:
+    header = blast.readline()
+
+if '\t' not in header:
+    sys.exit('\nError: BLAST data file is not tab delimited.\n')
+    
 
 
 # Create the empty lists IDs_fasta, headers_fasta, and seqs_fasta to store data
@@ -55,6 +109,9 @@ with open(fasta_file, 'r') as fasta:
         else:
             seqs_fasta.append(l)
 
+if len(headers_fasta) != len(seqs_fasta):
+    sys.exit('\nError: FASTA file corrupted. Unequal number of headers and '
+             'sequences found.\n')
 
 # Create the empty list data_blast. This list will store all the data from 
 # blast_file in a list, where every line is a list nested within data_blast.
@@ -83,8 +140,11 @@ with open(blast_file, 'r') as blast:
             data_blast.append(l.split('\t'))
 
 # Save the indecies of the columns used for generation of the output file.
-ID_blast_col = blast_header.index('#queryName')
-desc_blast_col = blast_header.index('hitDescription')
+ID_colname = '#queryName'
+desc_colname = 'hitDescription'
+
+ID_blast_col = blast_header.index(ID_colname)
+desc_blast_col = blast_header.index(desc_colname)
 
 
 # Create an empty dictionary to store protein IDs and their corresponding
@@ -95,63 +155,29 @@ custom_strings_dict = {}
 for i, row in enumerate(data_blast):
     
     # Assign the protein ID and its description to variables ID_blast and
-    # desc_blast and add them to the dictionary custom_strings_dict.
-    # if 'null' not in data_blast[i][desc_blast_col]:
+    # desc_blast and add them to the dictionary custom_strings_dict only if
+    # desc_blast does not contain 'null'.
     ID_blast = data_blast[i][ID_blast_col]
     desc_blast = data_blast[i][desc_blast_col]
-    custom_strings_dict[ID_blast] = (f'protein={desc_blast}\n')
+    if 'null' not in desc_blast:
+        custom_strings_dict[ID_blast] = (f'protein={desc_blast}\n')
+
         
-
-
-
-#IDs_blast = list(blast['#queryName'])
-
-# print(len(IDs_fasta), type(IDs_fasta))
-# print(len(headers_fasta), type(headers_fasta))
-# print(len(seqs_fasta), type(seqs_fasta))
-# print(len(IDs_blast), type(IDs_blast))
-
-# print(len(custom_strings_dict), type(custom_strings_dict))
-
-#IDs_fasta_set = set(IDs_fasta)
-#IDs_blast_set = set(IDs_blast)
-
-# data length mismatch:
-# len(ID_fasta_set) != len(ID_blast_set)
-
-# protein ID mismatch (only possible if there is no data length mismatch
-# len(IDs_fasta_set & IDs_blast_set) != len(ID_fasta_set)
-
-
-
-
-
-
-
-
-# Create the blank list null_IDs_blast to store the protein IDs from blast_file
-# whose entry in 'hitDescription' is null.
-null_IDs_blast = []
-
-# Loop through the keys of the dictionary custom_strings_dict.keys
-for k in custom_strings_dict.keys():
-    if 'null' in custom_strings_dict[k]:
-        null_IDs_blast.append(k)
-
-
-
-
-if (len(IDs_fasta) == len(headers_fasta) == len(seqs_fasta)
-    == len(custom_strings_dict)) != 1:
-    print('data length error')
-
-IDs_fasta_set = set(IDs_fasta)
-    
-
-
+# Open output_path provided by the user and write the output.
 with open(output_path, 'w') as output:
+    
+    # Loop through the list of fasta headers to write the output.
     for i, header_fasta in enumerate(headers_fasta):
-        if IDs_fasta[i] not in null_IDs_blast:
+        
+        # Check if the current ID is in the list of keys in the dictionary
+        # custom_strings_dict. If it is not, that protein IDs function was
+        # listed as 'null' and will not be included in the output file.
+        if IDs_fasta[i] in custom_strings_dict.keys():
+            
+            # Construct the output file.
             output.write(header_fasta
-                         + custom_strings_dict[IDs_fasta[i]])
-            output.write(seqs_fasta[i])
+                         + custom_strings_dict[IDs_fasta[i]]
+                         + seqs_fasta[i])
+
+# Print completion message.
+print(f'Output written to {output_path}')
