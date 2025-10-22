0b667b685e0ffaae56b8f405c0dc0e0268a4c2ba
otodreas <otodreas@gmail.com>
2025-10-22 16:10:50 +0200

    started AI excercise

---
+++ Modules/README.md
@@ -0,0 +1,10 @@
+# Modules structure
+
+Directory structure
+
+Modules/
+├─ .venv/
+├─ demo_modules/
+   ├─ notebooks ...
+
+activate the venv in Modules/, then navigate to demo_modules.
---
+++ Modules/demo_modules/src/bio_utils/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,21 @@
+from .sequence_utils import (
+    clean_dna,
+    base_counts,
+    find_motif,
+    kmer_counts,
+    hamming,
+    count_base,
+    revcomp,
+    gc_content,
+)
+
+__all__ = [
+    "clean_dna",
+    "base_counts",
+    "find_motif",
+    "kmer_counts",
+    "hamming",
+    "count_base",
+    "revcomp",
+    "gc_content",
+]
